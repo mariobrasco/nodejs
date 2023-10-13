71ad2d98c0d11c6c92b2857d1500b78327b8c2cd
mariobrasco <mariogaming1906@gmail.com>
2023-10-13 21:09:10 +0700

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(){
+    const[count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function tambah(){
+        setCount(count + 1);
+    }
+
+function kurang(){
+    setCount(count - 1);
+}
+function reset(){
+    setCount(0);
 }
 
-export default App
+return(
+    <>
+    <div className="container w-50 vh-100 d-flex justify-content-center align-items-center flex-column gap-3">
+        <h1 className="text-center fw-bold">{count}</h1>
+        <div className="d-flex justify-content-center align-content gap-3">
+            <button className="btn btn-primary" onClick={kurang}>
+                kurang
+            </button>
+            <button className="btn btn-success" onClick={tambah}>
+                tambah
+            </button>
+            <button className="btn btn-danger" onClick={reset}>
+                reset
+            </button>
+        </div>
+    </div>
+    </>
+);
+}
+
+export default App;
